ebd5138adf47d284c6839bd4b26c651f2bf89786
AndyCTS <Anindya.Bhattacharya4@cognizant.com>
2017-10-27 22:39:08 +0530

    Model classes changed

--- src/main/java/com/shoppingcart/order/model/OrderItem.java
+++ src/main/java/com/shoppingcart/order/model/OrderItem.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 /*
@@ -16,8 +19,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class OrderItem {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
+	@GeneratedValue(
+			strategy = GenerationType.AUTO,
+			generator="native"
+	)
+	@GenericGenerator(
+			name="native",
+			strategy="native"
+	)
 	private long itemId;
 	
 	private String name;
@@ -40,92 +49,154 @@ public class OrderItem {
 	private Long orderId;
 	
 	private int Quantity;
+	
+	
 
+	
 	public long getItemId() {
 		return itemId;
 	}
+
+
+
 
 	public void setItemId(long itemId) {
 		this.itemId = itemId;
 	}
 
+
+
+
 	public String getName() {
 		return name;
 	}
+
+
+
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+
+
+
 	public String getDescription() {
 		return description;
 	}
+
+
+
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+
+
+
 	public float getPrice() {
 		return price;
 	}
+
+
+
 
 	public void setPrice(float price) {
 		this.price = price;
 	}
 
+
+
+
 	public String getType() {
 		return type;
 	}
+
+
+
 
 	public void setType(String type) {
 		this.type = type;
 	}
 
+
+
+
 	public Date getCreatedDate() {
 		return createdDate;
 	}
+
+
+
 
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
 
+
+
+
 	public Date getModifiedDate() {
 		return modifiedDate;
 	}
+
+
+
 
 	public void setModifiedDate(Date modifiedDate) {
 		this.modifiedDate = modifiedDate;
 	}
 
+
+
+
 	public String getStatus() {
 		return status;
 	}
+
+
+
 
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
+
+
+
 	public Long getOrderId() {
 		return orderId;
 	}
 
+
+
+
 	public void setOrderId(Long orderId) {
 		this.orderId = orderId;
 	}
-	
-	public OrderItem() {
-		
-	}
+
+
+
 
 	public int getQuantity() {
 		return Quantity;
 	}
 
+
+
+
 	public void setQuantity(int quantity) {
 		Quantity = quantity;
 	}
-	
-	
+
+
+
+
+	public OrderItem() {
+		
+	}
+
 
 
 }
